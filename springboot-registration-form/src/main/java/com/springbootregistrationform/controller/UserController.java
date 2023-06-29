a480708ad8c0c68045835924df5b1e2cc0c4f6c4
Eric Maniraguha <44385819+ericmaniraguha@users.noreply.github.com>
2023-06-30 00:16:29 +0200

    Add files via upload
    The form is created in HTML, and linked to the home page.

--- springboot-registration-form/src/main/java/com/springbootregistrationform/controller/UserController.java
+++ springboot-registration-form/src/main/java/com/springbootregistrationform/controller/UserController.java
@@ -1,14 +1,27 @@
 package com.springbootregistrationform.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.springbootregistrationform.entity.User;
 
 @Controller
 public class UserController {
      
 	@GetMapping("/")
-	public String register() {
+	public String register(Model model) {
+		User user = new User();
+		model.addAttribute("user", user);
 		return "register";
 		
+	}
+	@PostMapping("/registerUser")
+	public String registerUser(@ModelAttribute("user") User user) 
+	{
+		System.out.println(user);
+		return "home";
 	}
 }
